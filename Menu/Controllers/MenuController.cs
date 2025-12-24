78d7257c8e45891f03d53bc829b45c41be39925c
Sugam Thapa <sugamthapa177@gmail.com>
2025-12-24 10:47:32 +0545

    Final Commit

--- Menu/Controllers/MenuController.cs
+++ Menu/Controllers/MenuController.cs
@@ -6,17 +6,25 @@ using System.Threading.Tasks;
 
 namespace Menu.Controllers
 {
-    public class Menu : Controller
+    public class MenuController : Controller
     {
         private readonly MenuContext _context;
 
-        public Menu(MenuContext context)
+        public MenuController(MenuContext context)
         {
             _context = context;
         }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View(await _context.Dishes.ToListAsync());
+            var dishes = from d in _context.Dishes
+                       select d;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                dishes = dishes.Where(d => d.Name.Contains(searchString));
+                return View(await dishes.ToListAsync());
+            }
+            return View(await dishes.ToListAsync());
         }
 
         public async Task<IActionResult> Details(int? id)
